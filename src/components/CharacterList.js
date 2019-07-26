747ea79a26ef3dcef06b08031d44393713d96011
Lisa <lmkirk@gmail.com>
2019-07-26 13:02:57 -0400

    characters is undefined, CharacterList line 27

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,31 +1,33 @@
+
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import CharacterCard from "./CharacterCard";
 
-export default function CharacterList(props) {
-  // TODO: Add useState to track data from useEffect
-  const [character, setCharacter] = useState(null);
-  const id = props.id;
+export default function CharacterList() {
+    // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState();
 
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-
-    axios.get(
-      "https://rickandmortyapi.com/api/character")
-      .then(results => {
-        console.log(results.data.id);
-        setCharacter(results.data.id);
-
-    })
+    axios
+      .get(`https://rickandmortyapi.com/api/character/`)
+    //  Important: verify the 2nd `useEffect` parameter: the dependencies array!
+      .then(response => {
+        setCharacters(response.data.results);
+        console.log(setCharacters);
+      })
       .catch(error => {
-        console.error('Error receiving Characters from API', error);
+        console.error(error);
       });
+  }, []);
 
+  return (
+    <section className='character-list grid-view'>
 
-}, []);
-
-  return <section className='character-list grid-view'>
-    <h2>TODO: `array.map()` over your state here!</h2>
+      <h2>TODO: `array.map()` over your state here!</h2>
+      {characters.map(character => (
+      <CharacterCard key={character.name} info={character}/>
+    ))}
   </section>
-
+);
 }
